baddd2673a5d99d3f1b1e608a53c8bdb12e67375
Jonas Soares <jonas.eng.software@gmail.com>
2021-07-20 04:45:23 -0300

    Animattion player check
    Add Animações no player
    > Parado ok
    > Andando (Left & Right) ok
    > Pulo ok

--- Jogo 2D de Plataforma/Assets/Scripts/Player.cs
+++ Jogo 2D de Plataforma/Assets/Scripts/Player.cs
@@ -7,11 +7,16 @@ public class Player : MonoBehaviour
     public float speed;
     public float JumpForce;
     private Rigidbody2D rig;
+    private Animator anim;
+
+    public bool isJumping;
+    public bool doubleJump;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         // Aqui o rig irá receber o componente Rigidbody que está anexado a esse script
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,17 +27,72 @@ public class Player : MonoBehaviour
     }
     void move()
     {// Função do Movimento horizontal
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f); 
+
         // Aqui a variavel Vector3 recebe uma nova variavel Vector3 com as posições 0 no y e 0 no z (0f em ambos)
-        // HOrizontal já tem algumas configurações pré determinadas dentro do prórpio Unity
+        // Horizontal já tem algumas configurações pré determinadas dentro do prórpio Unity
+
         transform.position += movement * Time.deltaTime * speed;
+
+        //Aqui nesses IF a condição de ser maior, menor ou igual a zero é quem determina se o MC está andando para direita
+        // esquerda ou simplesmente está parado respectivamente.
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("Walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f); // eulerAngels permite rotacionar o objeto 
+        }
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("Walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("Walk", false);
+        }
+
 
     }
     void Jump()
     {// Função do pulo vertical 
         if (Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                anim.SetBool("jump", true);
+            } //Se eu estiver pulando essa condição fica como verdadeira
+            else
+            {
+                if (doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce * 2f), ForceMode2D.Impulse);
+                    doubleJump = false;
+                    anim.SetBool("jump", false);
+                }// Se eu pressionar novamente o pulo caio nessa condição ---- Evita que haja "pulos infinitos no ar"
+            }
+           
         }
+
+    // Implementano o pulo duplo...
+    void OnCollisionEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.layer == 8)
+            {
+                isJumping = false;
+            }
+        }
+    
+    void OnCollisionExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.layer == 8)
+            {
+                isJumping = true;
+            }
+        }
+    // Fim da Implementação do pulo duplo
     }
 }
